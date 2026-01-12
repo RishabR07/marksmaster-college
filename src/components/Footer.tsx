07f8d8c9ecef8ce30c56ca393c432cb0df4d3636
RishabR07 <shettyrishab35@gmail.com>
2026-01-12 11:13:17 +0530

    changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -66,7 +66,7 @@ const Footer = () => {
             rel="noopener noreferrer"
             className="font-semibold hover:underline text-white"
           >
-            Mesnaldo
+            Sssssssssssssssssssssssss
           </a>
           . All Rights Reserved.
         </div>
